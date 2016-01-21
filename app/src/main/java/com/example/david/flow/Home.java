b81818dc1beea634f10ff207f1e7578589c840ba
ptroyard <pierre.troyard@gmail.com>
2016-01-21 16:24:07 +0100

    [Pierre] Home creation

--- app/src/main/java/com/example/david/flow/Home.java
+++ app/src/main/java/com/example/david/flow/Home.java
@@ -5,20 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
 
-    Button takevideo;
+    ImageButton shootButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        takevideo = (Button) findViewById(R.id.takevideo);
+        shootButton = (ImageButton) findViewById(R.id.shootButton);
 
 
-        takevideo.setOnClickListener(new View.OnClickListener() {
+        shootButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(Home.this, Shoot.class);
                 startActivityForResult(intent, 1);

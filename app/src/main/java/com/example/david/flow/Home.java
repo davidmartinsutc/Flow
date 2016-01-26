a3abd551d9ca10905c2da1ad707514af3a864f69
ptroyard <pierre.troyard@gmail.com>
2016-01-26 17:40:28 +0100

    [Pierre] Réseaux sociaux

--- app/src/main/java/com/example/david/flow/Home.java
+++ app/src/main/java/com/example/david/flow/Home.java
@@ -1,6 +1,7 @@
 package com.example.david.flow;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,8 +10,7 @@ import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
 
-    ImageButton shootButton;
-    ImageButton launchFlowButton;
+    ImageButton shootButton, launchFlowButton, twitterButton, fbButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,9 @@ public class Home extends AppCompatActivity {
 
         shootButton = (ImageButton) findViewById(R.id.shootButton);
         launchFlowButton = (ImageButton) findViewById(R.id.launchFlowButton);
+        twitterButton = (ImageButton) findViewById(R.id.twitterButton);
+        fbButton = (ImageButton) findViewById(R.id.fbButton);
+
 
         shootButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -34,7 +37,24 @@ public class Home extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+
+        twitterButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToUrl(getResources().getString(R.string.twitter_app_url));
+            }
+        });
+
+        fbButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToUrl(getResources().getString(R.string.fb_app_url));
+            }
+        });
     }
 
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
 
 }
